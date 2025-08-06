e3347c47b57c657c36d033fd54c812f17aab8521
Yousof <yousof.hosseinpour@gmail.com>
2025-08-06 17:49:02 +0330

    bug

--- Presentation/wwwroot/js/pages/project-details.js
+++ Presentation/wwwroot/js/pages/project-details.js
@@ -5,28 +5,32 @@ $('#itemModal').on('show.bs.modal', function (event) {
     const modal = $(this)
     let el = ``;
     let typeNum = type == "Estimate" ? 0 : 1
+    let removeBtn = "";
+    
     items.filter(i => i.ProjectDetailId == id && i.Type == typeNum).forEach(item => {
+        if(hasRemoveItemPermission)
+            removeBtn = `<button type="button" class="btn btn-danger deleteProjectDetailId" data-id="${item.Id}">حذف</button>`;
         el += `<tr>
             <td>${item.Title}</td>
             <td>${item.Area}</td>
             <td>${item.UnitPrice}</td>
             <td>${item.TotalPrice}</td>
             <td>${item.Description}</td>
-            <td>${hasRemoveItemPermission ? '<button type="button" class="btn btn-danger deleteProjectDetailId" data-id="${item.Id}">حذف</button>' : ''}</td>
+            <td>${removeBtn}</td>
         </tr>`
     })
     modal.find('table tbody').html(el)
-    
+
     modal.find('[name=ProjectDetailId]').val(id)
     modal.find('[name=Type]').val(type)
 })
-$("#editModal").on("show.bs.modal", function(event) {
+$("#editModal").on("show.bs.modal", function (event) {
     const button = $(event.relatedTarget)
     const id = button.data('id')
     const type = button.data('type')
-    const percent= button.data('percent')
+    const percent = button.data('percent')
     const date = button.data('date')
-    const description= button.data('description')
+    const description = button.data('description')
     const title = button.data('title')
     const modal = $(this)
     modal.find('[name=Id]').val(id)
@@ -36,15 +40,15 @@ $("#editModal").on("show.bs.modal", function(event) {
     modal.find('[name=Description]').val(description)
     modal.find('[name=Type]').val(type)
 })
-$('[name=UnitPrice], [name=Area]').on('keyup', function(){
-    
+$('[name=UnitPrice], [name=Area]').on('keyup', function () {
+
     let unit = $('[name=UnitPrice]').val() ?? 0;
     let area = $('[name=Area]').val() ?? 0;
     $('[name=TotalPrice]').val(unit * area);
-    
+
 })
 
-$('body').on("click", ".deleteProjectDetailId", function(){
+$('body').on("click", ".deleteProjectDetailId", function () {
 
     const id = $(this).data("id");
     const trEl = $(this).parents("tr")
